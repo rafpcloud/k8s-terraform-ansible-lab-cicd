51473e8ab9bb4abe8ec6093c56f97f2b8e422a49
rafpcloud <88331079+rafpcloud@users.noreply.github.com>
2021-12-21 18:06:45 -0300

    Update sg.tf
    libera  tudo  egress

--- sg.tf
+++ sg.tf
@@ -12,13 +12,14 @@ resource "aws_security_group" "sgweb" {
 
   }
 
+ 
   egress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
+    from_port       = 0
+    to_port         = 65535
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
   }
+  
 
   tags = {
     Name = "sgweb"
