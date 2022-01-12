4374f85e7375ff4b22377f5ca4d9137f00fde298
Pacheco <rafpcloud@gmail.com>
2022-01-12 00:34:08 -0300

    add route

--- route-table.tf
+++ route-table.tf
@@ -6,6 +6,11 @@ resource "aws_route_table" "xablau" {
     gateway_id = aws_internet_gateway.xablau-gw.id
   }
 
+  route {
+    cidr_block = "192.168.0.0/16"
+   
+  }
+
   tags = {
     Name = "Route Table Xablau"
   }
