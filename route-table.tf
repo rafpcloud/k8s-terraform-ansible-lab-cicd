9be6ee11f0ef67bd68433630d21148b0bf86f3e7
Pacheco <rafpcloud@gmail.com>
2022-01-12 10:37:25 -0300

    apagando arquivos desnecessarios

--- route-table.tf
+++ route-table.tf
@@ -6,12 +6,7 @@ resource "aws_route_table" "xablau" {
     gateway_id = aws_internet_gateway.xablau-gw.id
   }
 
-  route {
-    cidr_block = "192.168.0.0/16"
-   
-  }
-
-  tags = {
+   tags = {
     Name = "Route Table Xablau"
   }
 }
